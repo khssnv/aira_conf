a105f96550bee288a9acbacc42922d7ac70fd178
Alisher A. Khassanov <a.khssnv@gmail.com>
2019-03-25 18:12:37 +0400

    Configuration installation fix

--- install.sh
+++ install.sh
@@ -3,11 +3,22 @@
 if [ -e /etc/nixos/configuration.nix ];
 then
   mv /etc/nixos/configuration.nix /etc/nixos/configuration.nix.bkp
+  echo "Configuration backup at /etc/nixos/configuration.nix.bkp"
 fi
 
-cp -rf ./configuration /etc/nixos/
+echo "1 Replacing configuration..."
+cp -rf ./configuration/* /etc/nixos/
+
+echo "2 Implementing configuration..."
 nixos-rebuild switch
+
+echo "3 Please profile password for khassanov"
 passwd khassanov
+
+
+echo "4 Setting up dotfiles..."
 su khassanov
 cd ./dotfiles
 sh ./install.sh
+
+echo "5 Done."
